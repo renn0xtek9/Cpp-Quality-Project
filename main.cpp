2776c1d1cafb5de60f8516fb47549ff1abf2643a
m.haselbauer <m.haselbauer@samsung.com>
2019-10-21 08:40:31 +0200

    Reproduce same bug as when implementing pile in image_overlay_viewer

--- main.cpp
+++ main.cpp
@@ -8,46 +8,63 @@ struct whatever
     double ever;
 };
 
-
 cv::Mat CreateBlankImage()
 {
-    cv::Mat img(600,800,CV_8UC(3));
-    return img;
+    cv::Mat mat_ptr(600,800,CV_8UC(3));
+    return mat_ptr;
 }
-void DrawNumberOnFrame(cv::Mat img, int number)
+
+void DrawNumberOnFrame(cv::Mat& img, int number)
 {
     cv::Point origin;
     origin.x=100;
     origin.y=200;
-    std::string number_as_str=std::to_string(number);
-    cv::putText(img,number_as_str ,origin, cv::FONT_HERSHEY_COMPLEX_SMALL, 10.0, cv::Scalar(255,0,255),1,CV_AA); 
-    
+    std::string number_as_string=std::to_string(number);
+    cv::putText(img,number_as_string,origin, cv::FONT_HERSHEY_COMPLEX_SMALL, 10.0, cv::Scalar(255,0,255),1,CV_AA);
 }
 
 cv::Mat GetFrameFromSubscriber(int index)
 {
     cv::Mat ret=CreateBlankImage();
     DrawNumberOnFrame(ret,index);
-    return ret;
 }
 
-
-
 int main(int argc, char **argv) {
-    uint maxsize=3  ;    
-    std::queue<std::shared_ptr<cv::Mat>> finite_queue;
+//     uint maxsize=3  ;    
+//     std::queue<cv::Mat> finite_queue;
+//     std::shared_ptr<cv::Mat> m_frameptr;
 
     for(int i=0;i<15;i++)
-    {   
-        std::unique_ptr<cv::Mat> mat_ptr=std::make_unique<cv::Mat>(GetFrameFromSubscriber(i));
-        finite_queue.push(std::move(mat_ptr));
+    {
+         /*if (mat1)
+         {
+             std::cout<<"deal";
+             mat1->release();
+             mat1->deallocate();
+             delete mat1;
+             mat1=nullptr;
+
+         }*/
+//         std::unique_ptr<int> pt
+//         GetFrameFromSubscriber(mat1,i);
+        cv::Mat mat=GetFrameFromSubscriber(i);
+        std::cout<<"i"<<i<<"mat"<<&mat<<std::endl;
+        cv::imshow("mat_ptr",mat);
+//        cv::waitKey(0);
+//         m_frameptr=stnew cv::Mat(mat1->clone());
+//         m_frameptr=std::shared_ptr <cv::Mat>(new cv::Mat(mat1->clone()));
+        /*
+        finite_queue.emplace((*m_frameptr).clone());
         if (finite_queue.size()>maxsize)
         {
+            finite_queue.front().release();
             finite_queue.pop();
         }
-        std::shared_ptr<cv::Mat> display_mat=finite_queue.front();
+        cv  ::Mat clonedMat=finite_queue.front().clone();
+        std::unique_ptr<cv::Mat> display_mat=std::make_unique<cv::Mat>(clonedMat);
         cv::imshow("Display",*display_mat);
         cv::waitKey(0);
+        */
     }
     return 0;
 }
