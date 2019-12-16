9bd342ac9fb36d9ccf382e5678c73c0b69e508e4
m.haselbauer <m.haselbauer@samsung.com>
2019-12-16 14:10:00 +0100

    Remove unnecessary comment

--- main.cpp
+++ main.cpp
@@ -1,44 +1,8 @@
 #include <iostream>
 #include <probe.h>
 
-void ShortTime()
-{
-    std::cout<<"Short"<<std::endl;
-    for(int j=0;j<100;j++)
-    {
-        ;
-    }
-}
-void AllocateLikeMad()
-{
-    for(int index=0;index<100;index++)
-    {
-        int *ptr= new int;
-    }    
-}
-void AllocateALittle()
-{
-    for(int index=0;index<100;index++)
-    {
-        int *ptr= new int;
-    }    
-    
-}
-
-
-void SuperLongTime()
-{    
-    std::cout<<"Long"<<std::endl;
-    for(int i=0;i<100;i++)
-    {
-        ;
-    }
-}
 int main ( int argc, char **argv )
 {
     Probe();
-    ShortTime();
-    SuperLongTime();
-    AllocateLikeMad();
     return 0;
 }
