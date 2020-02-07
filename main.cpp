fcdf44c5c22f8f71d11b65c6ae5e9218ff83201b
maxime haselbauer <m.haselbauer@samsung.com>
2020-02-07 11:30:07 +0100

    Finish Prototpype implementation of how we want to refactor the ADAS_Node

--- main.cpp
+++ main.cpp
@@ -3,19 +3,24 @@
 #include <memory>
 
 
-
-struct DummyPayload{
+class DummyPayload{
+public:
+    static int instantiation_counter;
+    DummyPayload(int vara, double varb){
+        DummyPayload::instantiation_counter+=1;
+        var_a=vara;
+        var_b=varb;
+    }
     int var_a{};
     double var_b{};
 };
+int DummyPayload::instantiation_counter=0;
 
 template<class T> 
 class BaseClass
 {
 public:
-    BaseClass(){
-        
-    }
+    BaseClass() =default ;
     void on_Deactivate() {
         m_object.reset();
     }
@@ -34,7 +39,7 @@ private:
 class DerivedClass: public BaseClass<DummyPayload>
 {
 public:
-    DerivedClass(): BaseClass<DummyPayload>(){
+    DerivedClass(): BaseClass(){
         Instantiate();
     }
     void Instantiate() override{
@@ -52,6 +57,29 @@ int main ( int argc, char **argv )
     Probe();
     
     DerivedClass myobjec;
+    myobjec.on_Deactivate();
+    if(myobjec.m_object)
+    {
+        std::cout<<"Objec exist !!!"<<std::endl;
+        std::cout<<myobjec.m_object->var_a<<std::endl;
+    }
+    else
+    {
+        std::cout<<"Object Does not exist"<<std::endl;
+    }
+    
+    myobjec.on_Activate();
+    myobjec.on_Deactivate();
+    myobjec.on_Activate();
+     if(myobjec.m_object)
+    {
+        std::cout<<"Objec exist !!!"<<std::endl;
+        std::cout<<DummyPayload::instantiation_counter<<std::endl;
+    }
+    else
+    {
+        std::cout<<"Object Does not exist"<<std::endl;
+    }    
 
     
     
