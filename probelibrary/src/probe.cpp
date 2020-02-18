9b67c24e21e02249c42d5b184399ad5c898baebe
maxime haselbauer <m.haselbauer@samsung.com>
2020-02-18 15:09:12 +0100

    Add a cmake module to the project to format files with clang

--- probelibrary/src/probe.cpp
+++ probelibrary/src/probe.cpp
@@ -1,10 +1,8 @@
 #include "probe.h"
 #include <vector>
-void Probe ()
-{
-    std::vector<int> vec;
-    for(int i=0;i<100000;i++)
-    {
-        vec.emplace_back(0);
-    }
+void Probe() {
+  std::vector<int> vec;
+  for (int i = 0; i < 100000; i++) {
+    vec.emplace_back(0);
+  }
 }
--- probelibrary/tests/unit_test.cpp
+++ probelibrary/tests/unit_test.cpp
@@ -1,7 +1,6 @@
 #include <gtest/gtest.h>
 #include <probe.h>
 
-TEST(ProbeSanity ,NoThrow)
-{
-    EXPECT_NO_THROW(Probe());
+TEST(ProbeSanity, NoThrow) {
+  EXPECT_NO_THROW(Probe());
 }
