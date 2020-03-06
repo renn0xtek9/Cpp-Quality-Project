9df51617d7987be0c960b12059fcc4262f3daeb6
maxime haselbauer <m.haselbauer@samsung.com>
2020-03-06 15:56:58 +0100

    Change fromat cmake rule

--- cmake_modules/format_everything.cmake
+++ cmake_modules/format_everything.cmake
@@ -1,38 +1,42 @@
 # Additional targets to perform clang-format
 
-# Get all project files
-IF (NOT CHECK_CXX_SOURCE_FILES)
-  MESSAGE (
-    FATAL_ERROR
-      "Variable CHECK_CXX_SOURCE_FILES not defined - set it to the list of files to auto-format"
-  )
-  RETURN ()
-ENDIF ()
-
-FOREACH (FILE ${CHECK_CXX_SOURCE_FILES})
-  STRING (REGEX REPLACE "${CMAKE_CURRENT_SOURCE_DIR}"
-                        "${CMAKE_CURRENT_BINARY_DIR}" STAMPFILE ${FILE})
-  GET_FILENAME_COMPONENT (DIROFSTAMPFILE ${STAMPFILE} DIRECTORY)
-  ADD_CUSTOM_COMMAND (
-    OUTPUT ${STAMPFILE}.stamp
-    DEPENDS ${FILE}
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${DIROFSTAMPFILE}
-    COMMAND /usr/bin/clang-format -i ${FILE}
-    COMMAND ${CMAKE_COMMAND} -E touch "${STAMPFILE}.stamp"
-    COMMENT "Formatting ${FILE} and stamping it with ${STAMPFILE}.stamp"
-    VERBATIM)
-  LIST (APPEND FORMAT_DEPENDENCIES ${STAMPFILE}.stamp)
-ENDFOREACH (FILE)
-# BUG if a file get renamed, its dependencies remains and will create make[2]:
-# *** No rule to make target '../probelibrary/include/wtf.h', needed by
+# Get all project files IF (NOT CHECK_CXX_SOURCE_FILES) MESSAGE ( FATAL_ERROR
+# "Variable CHECK_CXX_SOURCE_FILES not defined - set it to the list of files to
+# auto-format" ) RETURN () ENDIF ()
+#
+# FOREACH (FILE ${CHECK_CXX_SOURCE_FILES}) STRING (REGEX REPLACE
+# "${CMAKE_CURRENT_SOURCE_DIR}" "${CMAKE_CURRENT_BINARY_DIR}" STAMPFILE ${FILE})
+# GET_FILENAME_COMPONENT (DIROFSTAMPFILE ${STAMPFILE} DIRECTORY)
+# ADD_CUSTOM_COMMAND ( OUTPUT ${STAMPFILE}.stamp DEPENDS ${FILE} COMMAND
+# ${CMAKE_COMMAND} -E make_directory ${DIROFSTAMPFILE} COMMAND /usr/bin/clang-
+# format -i ${FILE} COMMAND ${CMAKE_COMMAND} -E touch "${STAMPFILE}.stamp"
+# COMMENT "Formatting ${FILE} and stamping it with ${STAMPFILE}.stamp" VERBATIM)
+# LIST (APPEND FORMAT_DEPENDENCIES ${STAMPFILE}.stamp) ENDFOREACH (FILE) BUG if
+# a file get renamed, its dependencies remains and will create make[2]: *** No
+# rule to make target '../probelibrary/include/wtf.h', needed by
 # 'probelibrary/include/wtf.h.stamp'.  Stop.
-ADD_CUSTOM_TARGET (format ALL DEPENDS ${FORMAT_DEPENDENCIES}
-                                      ${BUILDDIRECTORY}/formatrules.txt)
+
+ADD_CUSTOM_TARGET (
+  create-format-rule ALL
+  COMMENT "Create format make rule"
+  DEPENDS
+    ${CMAKE_BINARY_DIR}/CMakeFiles/create-format-rule.dir/create-format-rule-creation.log
+)
 
 ADD_CUSTOM_COMMAND (
-  OUTPUT ${BUILDDIRECTORY}/formatrules.txt
+  OUTPUT
+    ${CMAKE_BINARY_DIR}/CMakeFiles/create-format-rule.dir/create-format-rule-creation.log
+    # OUTPUT ${CMAKE_BINARY_DIR}/CMakeFiles/format.dir/build.make
   WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-  COMMAND echo "generate rules----------"
+  COMMAND cmake -E make_directory ${CMAKE_BINARY_DIR}/CMakeFiles/format.dir
+  COMMAND echo "Generate format rules"
   COMMAND
     python3 ${CMAKE_SOURCE_DIR}/cmake_modules/scripts/generate_format_rules.py
-    "${BUILDDIRECTORY}" >${BUILDDIRECTORY}/formatrules.txt)
+    --build-directory ${CMAKE_BINARY_DIR} --repository ${CMAKE_SOURCE_DIR}
+    --cpp-format-tool "/usr/bin/clang-format -i"
+    >${CMAKE_BINARY_DIR}/CMakeFiles/create-format-rule.dir/create-format-rule-creation.log
+)
+
+ADD_CUSTOM_TARGET (
+  format ALL # DEPENDS ${CMAKE_BINARY_DIR}/CMakeFiles/format.dir/build.make
+  DEPENDS create-format-rule)
