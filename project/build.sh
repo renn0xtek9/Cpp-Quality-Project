f50d6c503434d297b25e104415308657f0c2e817
renn0xtek9 <renn0xtek9@laposte.net>
2020-03-25 00:24:55 +0100

    Add a red test for the rules that get version from git hash

--- project/build.sh
+++ project/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 mkdir -p build
 cmake -G "Unix Makefiles" --build build -DCMAKE_BUILD_TYPE=Debug .
-make 
+make -j 12
--- workflow_test/git_hash_fresh_configure.sh
+++ workflow_test/git_hash_fresh_configure.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-exit 1
+set -e
+cd ..
+TMP=$(mktemp -d)
+cp -r project $TMP
+cd $TMP 
+cd project
+./build.sh
+if [[ ! -f "version.hpp" ]]
+then
+    echo "Could not find version.hpp"
+    exit 1
+fi
+version=$(cat "version.hpp" |grep "#define VERSION" | sed 's/#define VERSION//g' |sed 's/[[:space:]]//g')
+
+if [ -z "$version" ] 
+then
+    echo "Version number is empty in version.hpp"
+    exit 2 
+fi
+
