82f0c0bf3ad30ab174a4febd54204fb2bf36d22f
Maxime Haselbauer <renn0xtek9@laposte.net>
2023-04-10 12:51:07 +0200

    Remve the project subfolder

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash 
 set -e 
+rm -rf build
 mkdir -p build
 cd build 
+conan install ../conanfile.txt --profile:build=../conan_profiles/linux-x86   --profile:host=../conan_profiles/linux-x86 --build missing
 cmake ..
 make 
 make test
