17dcc14e9e1e1cf4366af9fbd5d79ed8105e7c6d
Maxime Haselbauer <renn0xtek9@laposte.net>
2023-04-13 17:19:10 +0000

    Add CI workflow

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
 set -e 
 rm -rf build
 mkdir -p build
--- scripts/setup_development_environment.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-set -e 
-python3 -m pip install -r requirements.txt
-pre-commit install
