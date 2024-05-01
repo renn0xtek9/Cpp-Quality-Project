d0be0d2fd73131551b22ef3071d42ef3066d9be8
Maxime Haselbauer <renn0xtek9@laposte.net>
2024-05-01 09:13:15 +0000

    Fix basic demo test

--- test/test_script.sh
+++ test/test_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./testexe o
+./bin/testexe o
 if [[ $? != "0" ]] ; then exit 1; fi
-./test_gtest_based --gtest_filter=This*
+./bin/test_gtest_based --gtest_filter=This*
 if [[ $? != "0" ]] ; then exit 1; fi
