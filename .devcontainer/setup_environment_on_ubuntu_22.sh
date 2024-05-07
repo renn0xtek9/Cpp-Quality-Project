98fffc57b309912eb28c39a4d5da6793232b23d2
Maxime Haselbauer <maxime.haselbauer@luminartech.com>
2024-05-07 15:14:34 +0000

    Fix installation being exectued from a differente folder

--- .devcontainer/setup_environment_on_ubuntu_22.sh
+++ .devcontainer/setup_environment_on_ubuntu_22.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 set -euxo pipefail
 DEBIAN_FRONTEND=noninteractive
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
 sudo apt-get update
 sudo apt-get install -y curl nano cmake gcc gcovr python3 python3-pip git texlive-full biber
 
@@ -12,5 +14,3 @@ sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null 
 sudo apt update
 sudo apt install gh -y
-
-rm -rf /var/lib/apt/lists/*
