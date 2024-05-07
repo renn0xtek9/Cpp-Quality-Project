23b95a76f71926f6aa3932742a04b5ac3ffcefbc
renn0xtek9 <renn0xtek9@laposte.net>
2024-05-07 14:09:23 +0000

    Setup CI pipelines

--- .devcontainer/setup_environment_on_ubuntu_22.sh
+++ .devcontainer/setup_environment_on_ubuntu_22.sh
@@ -1,8 +1,10 @@
 #!/bin/bash 
 set -euxo pipefail
 DEBIAN_FRONTEND=noninteractive
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
 sudo apt-get update
-sudo apt-get install -y curl nano cmake gcc python3 python3-pip git texlive-full biber
+sudo apt-get install -y curl nano cmake gcc gcovr python3 python3-pip git texlive-full biber
 
 python3 -m pip install -r requirements.txt
 
@@ -12,5 +14,3 @@ sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null 
 sudo apt update
 sudo apt install gh -y
-
-rm -rf /var/lib/apt/lists/*
