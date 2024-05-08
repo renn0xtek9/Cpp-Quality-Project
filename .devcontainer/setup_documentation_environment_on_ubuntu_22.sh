8651be345923ed6e9b2b2400c092b11ca9dc5c89
Maxime Haselbauer <maxime.haselbauer@luminartech.com>
2024-05-08 14:05:19 +0000

    Fix dependencies in doxygen documentation

--- .devcontainer/setup_documentation_environment_on_ubuntu_22.sh
+++ .devcontainer/setup_documentation_environment_on_ubuntu_22.sh
@@ -4,4 +4,4 @@ DEBIAN_FRONTEND=noninteractive
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 sudo apt-get update
-sudo apt-get install -y cmake git texlive-full biber doxygen graphviz
+sudo apt-get install -y cmake git texlive-full biber doxygen graphviz makefile2graph
