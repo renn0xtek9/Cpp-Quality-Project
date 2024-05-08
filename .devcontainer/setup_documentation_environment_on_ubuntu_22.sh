692ae929a8a46ef306e26e640cfe56027c0cfa6d
Maxime Haselbauer <maxime.haselbauer@luminartech.com>
2024-05-08 15:41:42 +0000

    Add conan in the documentation pipeline for configuring

--- .devcontainer/setup_documentation_environment_on_ubuntu_22.sh
+++ .devcontainer/setup_documentation_environment_on_ubuntu_22.sh
@@ -5,3 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 sudo apt-get update
 sudo apt-get install -y cmake git texlive-full biber doxygen graphviz makefile2graph
+python3 -m pip install -r requirements.txt
